30addc1bd56a2ada40cfcd97be0d08550429e032
Szabadkia Márk <mqrelly@gmail.com>
2012-07-17 22:56:51 +0200

    Initial commit.
    - Bare railse generated app.
    - Gemfile customized.
    - Using RSpec instead of Test::Unit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ror3tutorialSmapleApp::Application.config.secret_token = '1a611c0ac329b20cffda47f6320af7a50a21bf4e209e1a21b9e942d2c19e11dc09d2ac22b00b7b1dc18aa3126cfbc35aee3b9d5847d55f1a23171fbfd60f6229'
