c94bf61f03391bfa0c51724c607336562b750ae7
Szabadkia Márk <mqrelly@gmail.com>
2012-07-18 14:33:41 +0200

    Add 'about' static page.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,7 @@ describe "Static Pages" do
   describe "Home page" do
     it "should have the content 'Smaple App'" do
       visit "/static_pages/home"
-      page.should have_content("Sample App")
+      page.should have_content "Sample App"
     end
   end
 
@@ -13,6 +13,13 @@ describe "Static Pages" do
     it "should have the content 'Help'" do
       visit "/static_pages/help"
       page.should have_content "Help"
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit "/static_pages/about"
+      page.should have_content "About Us"
     end
   end
 end
