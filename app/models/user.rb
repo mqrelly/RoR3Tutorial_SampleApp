94437cbcedb86f322ff583c8e94be56911a8ce90
Szabadkia Márk <mqrelly@gmail.com>
2012-07-20 12:12:44 +0200

    Add secure password to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
 
   validates :name, :presence => true, :length => { :maximum => 50 }
 
@@ -8,6 +9,10 @@ class User < ActiveRecord::Base
     :presence => true, 
     :format => { :with => VALID_EMAIL_REGEX },
     :uniqueness => { :case_sensitive => false }
+  validates :password,
+    :presence => true,
+    :length => { :minimum => 6 }
+  validates :password_confirmation, :presence => true
 
   before_save { |user| user.email = email.downcase }
 end
