ebb60e754e125bd56eaa99b5038d8b34da468bd1
Szabadkia Márk <mqrelly@gmail.com>
2012-07-24 06:52:40 +0200

    Add signup error message specs.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,6 +17,20 @@ describe "User pages" do
         it "should not create a user" do
           expect { click_button submit }.not_to change(User, :count)
         end
+
+        describe "after submission" do
+          before { click_button submit }
+
+          it { should have_selector "title", text: "Sign up" }
+          it { should have_selector "div", text: /The form contains \d* error(s?)/ }
+          it { should have_selector "li", text: "Password digest can't be blank" }
+          it { should have_selector "li", text: "Name can't be blank" }
+          it { should have_selector "li", text: "Email can't be blank" }
+          it { should have_selector "li", text: "Email is invalid" }
+          it { should have_selector "li", text: "Password can't be blank" }
+          it { should have_selector "li", text: "Password is too short" }
+          it { should have_selector "li", text: "Password confirmation can't be blank" }
+        end
       end
 
       describe "with valid information" do
