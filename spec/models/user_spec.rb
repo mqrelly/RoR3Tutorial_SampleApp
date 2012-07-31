38510efd8d8009337b129ff11af568c9a735d621
Szabadkia Márk <mqrelly@gmail.com>
2012-07-31 06:36:09 +0200

    Add Users#destroy with admin privs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,7 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:remember_token) }
   it { should be_valid }
+  it { should_not be_admin }
 
   describe "when name is not present" do
     before { @user.name = "" }
@@ -103,5 +104,14 @@ describe User do
   describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
+  end
+
+  describe "with 'admin' attribute set to true" do
+    before do
+      @user.save!
+      @user.toggle! :admin
+    end
+
+    it { should be_admin }
   end
 end
