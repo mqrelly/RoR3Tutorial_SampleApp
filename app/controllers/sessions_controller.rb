97c17307ecd69c5320a80649de3ad34b338d9f82
Szabadkia Márk <mqrelly@gmail.com>
2012-07-31 05:05:01 +0200

    Add friendli forwarding.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email email
     if user && user.authenticate(password)
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:error] = "Invalid email/password combination"
       render "new"
