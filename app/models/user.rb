da8e82d09c2a4a4df3f463cf8f6b5d3ea7f2cb0d
Szabadkia Márk <mqrelly@gmail.com>
2012-09-19 10:43:22 +0200

    Add Micropost model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
+  has_many :microposts
 
   validates :name, :presence => true, :length => { :maximum => 50 }
 
