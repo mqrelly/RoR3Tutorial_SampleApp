dce398d2997cbb2709a6aa718212db0b8e38b9cd
Szabadkia Márk <mqrelly@gmail.com>
2012-09-26 09:46:33 +0200

    Add Relationship model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
 
   validates :name, :presence => true, :length => { :maximum => 50 }
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,8 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
   it { should be_valid }
   it { should_not be_admin }
 
